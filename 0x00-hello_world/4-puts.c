57e74f29521af69ddf58a1e01d0058fe5792cfee
Adeshokan Fashola <boyshokan@gmail.com>
2022-04-07 05:02:14 -0700

    4-puts.c comments

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,20 @@
 #include<stdio.h>
 
 /**
- *  Main -Entry point
- *  Return: Always 0 (Success)
  *
- */
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Return: Always 0 (Success)
+ *
+ *     */
 
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+
+		puts("\"Programming is like building a multilingual puzzle");
+
+			return (0);
+
 }
