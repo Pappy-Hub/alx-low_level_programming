6e016259bd3fd1e89ce4cfc452c90b513904da38
Adeshokan Fashola <boyshokan@gmail.com>
2022-04-07 08:27:38 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,11 +3,11 @@
 /**
  * main - main block
  *
- * * Return: Always 0 (Success)
+ * Return: Always 0 (Success)
  */
 
-int main (void) 
+int main (void)
 {
-puts ("\"Programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
