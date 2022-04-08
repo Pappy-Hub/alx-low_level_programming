c0b0f904accd072c33544884dd142a066301aeeb
Adeshokan Fashola <boyshokan@gmail.com>
2022-04-08 06:30:36 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -11,12 +11,9 @@
 int main(void)
 {
 	int n;
-
-
+	
 	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-
-
+        n = rand() - RAND_MAX / 2;
 	if (n > 0)
 	{
 	        printf("%d is positive\n", n);
@@ -25,8 +22,8 @@ int main(void)
 	{
 		printf("%d is zero\n", n);
 	}
-        else
+	else
 	{
-	        printf("%d is negative\n", n);
+		printf("%d is negative\n", n);
 	}
         return (0);
