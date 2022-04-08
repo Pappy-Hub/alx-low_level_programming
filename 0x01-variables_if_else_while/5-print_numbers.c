6885c23833edb156b2d6a2929f2b61003c369178
Adeshokan Fashola <boyshokan@gmail.com>
2022-04-08 08:43:29 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+
+{
+int pappy;
+
+for (pappy = 0 ; pappy <10; pappy++)
+{
+printf("%pappy", pappy);
+}
+
+printf("\n");
+
+return (0);
+
+}
