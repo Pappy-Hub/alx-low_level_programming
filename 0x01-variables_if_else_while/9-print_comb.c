53f2cb431bbbeb1f6a7e0c3f752c2e91f83ebebc
Adeshokan Fashola <boyshokan@gmail.com>
2022-04-08 12:43:48 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,11 +11,11 @@ int main(void)
 
 	for (i = 48; i < 58; i++)
 	{
-		putcha(i);
+		putchar(i);
 		if (i != 57)
 		{
-		putchar (',');
-		putchar (' ');
+		putchar(',');
+		putchar(' ');
 		}
 	}
 	putchar('\n');
