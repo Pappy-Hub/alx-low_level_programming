609c643cf319b371edff9767035bc81719b8b529
Adeshokan Fashola <boyshokan@gmail.com>
2022-04-08 12:31:19 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+
+{
+	int i;
+
+	for (i = 48; i = 58; i++)
+	{
+		putcha(i);
+		if (i != 57)
+		{
+		putcha (',');
+		putcha (',');
+		}
+	}
+	putchar('\n')
+	return (0);
+}
