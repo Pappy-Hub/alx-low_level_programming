c4693cdeb670deb67f9ab23e5b7d22c8c0ac77bd
Adeshokan Fashola <boyshokan@gmail.com>
2022-04-08 08:56:37 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Result: Always 0 (Success)
+ */
+
+int main(void)
+
+{
+int i;
+
+for (i = 48; i <= 57; i++)
+{
+putchar (i);
+}
+
+putchar ('\n');
+
+return (0);
+
+}
+
+
