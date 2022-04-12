90c46f416113f6ccf024a664875c946c3d2b6e51
Adeshokan Fashola <boyshokan@gmail.com>
2022-04-12 12:34:11 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -4,7 +4,7 @@
  * print_times_table - prints the times table
  * @n: integer for which the times table will be printed
  *
- * Description: prints the times table
+ * Description: Prints the times table
  *
  * Return: void
  */
