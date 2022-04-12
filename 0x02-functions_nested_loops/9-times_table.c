1885d32a8804c469b088bf0d2da34bb10f2624d7
Adeshokan Fashola <boyshokan@gmail.com>
2022-04-12 06:30:14 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,21 +9,17 @@
 void times_table(void)
 
 {
-
 	int a;
 	int b;
 	int c;
 
 	for (a = 0; a <= 9; a++)
-
 	{
 		for (b = 0; b <= 9; b++)
-
 		{
 			c = a * b;
 
 			if ((c / 10) == 0)
-
 			{
 				if (b == 0)
 
@@ -32,11 +28,9 @@ void times_table(void)
 				}
 
 				if (b != 0)
-
 				{
 					_putchar (' ');
 					_putchar ((c % 10) + '0');
-
 				}
 
 				if (b < 9)
@@ -44,10 +38,8 @@ void times_table(void)
 					_putchar(',');
 					_putchar (' ');
 				}
-
 			}
 			else
-
 			{
 				_putchar ((c / 10) + '0');
 				_putchar ((c % 10) + '0');
