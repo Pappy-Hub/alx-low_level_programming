1dd250b74926db7f0378404143a9801dc760374e
Pappy <65431875+Pappy-Hub@users.noreply.github.com>
2022-04-28 18:14:17 +0100

    Update 0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -12,8 +12,7 @@ void _puts_recursion(char *s)
 
 {
 
-	if (*s == '\0')
-
+	if (*s == '\0')	
 		_putchar('\n');
 
 	else
@@ -27,3 +26,31 @@ void _puts_recursion(char *s)
 	}
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+		
+
+
+
+	
+
+		
+
+		
+
+	
+
+
